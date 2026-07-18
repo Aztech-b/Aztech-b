c623f9378375b9a1e49bd1f7230cf5d6ab9b6946
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-07-19 03:07:08 +0500

    added webGL check

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,26 +9,48 @@ function Main() {
 	const bgY = useTransform(scrollY, [0, 1000], [0, 300], { clamp: true });
 	const whiteColor = "#fff";
 	const darkColor = "rgba(0,0,0,0)";
+	const isWebGLon = () => {
+		try {
+			const canvas = document.createElement("canvas");
+			canvas.id = "webglTestCanvas";
+			return !!(
+				window.WebGLRenderingContext &&
+				(canvas.getContext("webgl") || canvas.getContext("experimental-webgl"))
+			);
+		} catch (e) {
+			return false;
+		} finally {
+			try {
+				document.removeChild(document.querySelector("#webglTestCanvas"));
+			} catch {}
+		}
+	};
 	return (
 		<div className="relative backgroundContainer">
-			<motion.div className="absolute inset-0 -z-10 items-center" style={{ y: bgY }}>
-				<SoftAurora
-					speed={0.6}
-					scale={1.5}
-					brightness={1}
-					color1="#6633ee"
-					color2="#e100ff"
-					noiseFrequency={2.5}
-					noiseAmplitude={1}
-					bandHeight={0.5}
-					bandSpread={1}
-					octaveDecay={0.1}
-					layerOffset={0}
-					colorSpeed={1}
-					enableMouseInteraction
-					mouseInfluence={0.25}
-				/>
-			</motion.div>
+			{isWebGLon() ? (
+				<motion.div className="absolute inset-0 -z-10 items-center" style={{ y: bgY }}>
+					<SoftAurora
+						speed={0.6}
+						scale={1.5}
+						brightness={1}
+						color1="#6633ee"
+						color2="#e100ff"
+						noiseFrequency={2.5}
+						noiseAmplitude={1}
+						bandHeight={0.5}
+						bandSpread={1}
+						octaveDecay={0.1}
+						layerOffset={0}
+						colorSpeed={1}
+						enableMouseInteraction
+						mouseInfluence={0.25}
+					/>
+				</motion.div>
+			) : (
+				<div
+					className={`absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_75%,var(--background)_40%,#63e_100%)]`}
+				></div>
+			)}
 			<div className={`content`}>
 				<div className={styles.main}>
 					<motion.div
