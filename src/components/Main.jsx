00d715b912d247c803c033d42486bb802cffb103
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-07-18 19:24:34 +0500

    added background in main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,22 +6,41 @@ function Main() {
     const whiteColor = "#fff";
     const darkColor = "#000";
     return (
-        <>
-            <div className={styles.main}>
-                <motion.div
-                    initial={{ "--color-1": whiteColor, "--color-2": whiteColor }}
-                    animate={{
-                        "--color-1": [whiteColor, whiteColor, whiteColor, darkColor, darkColor, whiteColor, whiteColor],
-                        "--color-2": [darkColor, darkColor, whiteColor, whiteColor, whiteColor, whiteColor, darkColor],
-                        transition: { ease: "easeIn", duration: 10, repeatType: "loop", repeat: Infinity },
-                    }}
-                    className={styles.box}
-                >
-                    <div className={styles.name}>Bakdaulet</div>
-                </motion.div>
-                <ChevronDown size={60}></ChevronDown>
+        <div className="relative backgroundContainer">
+            <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_75%,var(--background)_40%,#63e_100%)]"></div>
+            <div className={`content`}>
+                <div className={styles.main}>
+                    <motion.div
+                        initial={{ "--color-1": whiteColor, "--color-2": whiteColor }}
+                        animate={{
+                            "--color-1": [
+                                whiteColor,
+                                whiteColor,
+                                whiteColor,
+                                darkColor,
+                                darkColor,
+                                whiteColor,
+                                whiteColor,
+                            ],
+                            "--color-2": [
+                                darkColor,
+                                darkColor,
+                                whiteColor,
+                                whiteColor,
+                                whiteColor,
+                                whiteColor,
+                                darkColor,
+                            ],
+                            transition: { ease: "easeIn", duration: 10, repeatType: "loop", repeat: Infinity },
+                        }}
+                        className={styles.box}
+                    >
+                        <div className={styles.name}>Bakdaulet</div>
+                    </motion.div>
+                    <ChevronDown size={60}></ChevronDown>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
 
