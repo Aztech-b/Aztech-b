ffe8297559e6af8e1731928b318822a8c72f9b8c
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-07-22 21:49:33 +0500

    scroll thing in featured projects is inside of comtent(not in the side with fixed posiiton as it used to be

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -75,73 +75,68 @@ function FeaturedProjects() {
     return (
         <div className="content">
             <Divider className={"mt-20"}>Featured Projects</Divider>
-            <div className="p-9 flex items-start gap-9" ref={container}>
-                <MotionCard
-                    layout
-                    transition={{ ease: [0.4, 0, 0.2, 1], duration: 0.35 }}
-                    className={"sticky top-60 w-[130%] overflow-hidden"}
-                >
-                    <AnimatePresence mode="popLayout">
+            <div className="p-9 flex items-start gap-4" ref={container}>
+                <div className={"sticky top-60 w-[130%] h-full flex gap-5"}>
+                    <MotionCard
+                        layout
+                        transition={{ ease: [0.4, 0, 0.2, 1], duration: 0.35 }}
+                        className={"overflow-hidden flex-1 h-max self-center"}
+                    >
+                        <AnimatePresence mode="popLayout">
+                            <motion.div
+                                key={data.title}
+                                layout="position"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.2, ease: [0.4, 0, 0.2, 1] }}
+                            >
+                                <CardHeader>
+                                    <CardTitle className="text-center mb-0 text-[clamp(1rem,4vw,3.5rem)]">
+                                        {data.title}
+                                    </CardTitle>
+                                    <CardDescription>{data.description}</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="flex flex-wrap gap-4 align-middle justify-center my-5">
+                                        {data.chips.map((chip, index) => (
+                                            <Badge key={index}>
+                                                {chip.icon}
+                                                {chip.label}
+                                            </Badge>
+                                        ))}
+                                    </div>
+                                </CardContent>
+                                <CardFooter className="grid grid-cols-[repeat(auto-fit,minmax(min-content,1fr))] gap-4">
+                                    {/* <div className="grid grid-cols-[repeat(auto-fit,minmax(40px,1fr))] gap-4 mt-12"> */}
+                                    {!data.links ? (
+                                        <h2 className="text-center">Coming Soon...</h2>
+                                    ) : (
+                                        <>
+                                            <Button variant="outline" href={data.links.code}>
+                                                GitHub
+                                            </Button>
+                                            <Button href={data.links.live}>See It Live</Button>
+                                        </>
+                                    )}
+                                    {/* </div> */}
+                                </CardFooter>
+                            </motion.div>
+                        </AnimatePresence>
+                    </MotionCard>
+                    <div className="w-3 h-full relative">
                         <motion.div
-                            key={data.title}
-                            layout="position"
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.2, ease: [0.4, 0, 0.2, 1] }}
-                        >
-                            <CardHeader>
-                                <CardTitle className="text-center mb-0 text-[clamp(1rem,4vw,3.5rem)]">
-                                    {data.title}
-                                </CardTitle>
-                                <CardDescription>{data.description}</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <div className="flex flex-wrap gap-4 align-middle justify-center my-5">
-                                    {data.chips.map((chip, index) => (
-                                        <Badge key={index}>
-                                            {chip.icon}
-                                            {chip.label}
-                                        </Badge>
-                                    ))}
-                                </div>
-                            </CardContent>
-                            <CardFooter className="grid grid-cols-[repeat(auto-fit,minmax(min-content,1fr))] gap-4">
-                                {/* <div className="grid grid-cols-[repeat(auto-fit,minmax(40px,1fr))] gap-4 mt-12"> */}
-                                {!data.links ? (
-                                    <h2 className="text-center">Coming Soon...</h2>
-                                ) : (
-                                    <>
-                                        <Button variant="outline" href={data.links.code}>
-                                            GitHub
-                                        </Button>
-                                        <Button href={data.links.live}>See It Live</Button>
-                                    </>
-                                )}
-                                {/* </div> */}
-                            </CardFooter>
-                        </motion.div>
-                    </AnimatePresence>
-                </MotionCard>
+                            style={{ scaleY: scrollYProgress, height: "300px", originY: 0 }}
+                            className="sticky self-stretch top-60 min-h-full max-h-full w-full bg-amber-400"
+                        ></motion.div>
+                    </div>
+                </div>
+
                 <div className="flex flex-col gap-20 projectsContainer">
                     {projectsData.map((project, index) => (
                         <Project key={index} data={project}></Project>
                     ))}
                 </div>
-                <motion.div
-                    style={{
-                        scaleY: scrollYProgress,
-                        position: "fixed",
-                        top: 0,
-                        right: 0,
-                        bottom: 0,
-                        width: 10,
-                        originY: 0,
-                    }}
-                    className="h-full bg-amber-400"
-                >
-                    {scrollYProgress}
-                </motion.div>
             </div>
         </div>
     );
