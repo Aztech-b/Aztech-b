d8780e9eb82eccbae5469397634ba743f9a40595
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-07-25 13:14:33 +0500

    fixed curtain transition animating after clicking the same location transition link and navigating to another non transiiton link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from "motion/react";
-import { createContext, Suspense, useContext, useState } from "react";
+import { createContext, Suspense, useContext, useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router";
 import Curtain from "./Curtain";
 import Footer from "./Footer";
@@ -12,6 +12,15 @@ export const useTransitionContext = () => useContext(TransitionContextProvider);
 function App() {
     const [targetLocation, setTargetLocation] = useState(null);
     const location = useLocation();
+
+    // when transition link is clicked but the target location and current location are identical
+    // conditional rendering inside app component waits for location and target location not to be identical
+    // as target location state is not null, any link change(even if the link is not transition link) triggers the curtain animation
+    useEffect(() => {
+        if (location.pathname === targetLocation) {
+            setTargetLocation(null);
+        }
+    }, [location.pathname, targetLocation]);
 
     return (
         <main className="relative">
