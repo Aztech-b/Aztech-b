52b98339e2257382e8467e5071fda39857491097
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-07-30 23:00:42 +0500

    added lazy loading everything

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
 import { StrictMode, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
 import App from "./components/App";
 import "./i18n/config";
-import MyPoemsIndex from "./pages/MyPoemsIndex";
-import Poem from "./pages/Poem";
 import "./styles/global.css";
 import "./styles/main.css";
 import "./styles/viewTransitions.css";
 
 const MyPoems = lazy(() => import("./pages/MyPoems"));
 const Index = lazy(() => import("./pages/Index"));
+const MyPoemsIndex = lazy(() => import("./pages/MyPoemsIndex"));
+const Poem = lazy(() => import("./pages/Poem"));
 
 const router = createBrowserRouter([
     {
