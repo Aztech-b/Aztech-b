94e3308280476127210e3b9e0c04b99a68a8b088
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-07-30 23:49:28 +0500

    made tools languge card responsive

--- src/components/ToolsAndSkills.jsx
+++ src/components/ToolsAndSkills.jsx
@@ -55,28 +55,26 @@ const languages = [
 function ToolsAndSkills() {
     const { t } = useTranslation();
     return (
-        <div className={"main content"}>
+        <div className={"main content px-2"}>
             <Divider className={"mb-8 mt-16"} textAlign="left">
                 <h2>{t("indexPage.toolsAndSkills.header")}</h2>
             </Divider>
-            <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center align-baseline justify-items-center w-full gap-8 px-2">
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center align-baseline justify-items-center w-full gap-8">
                 {data.map((data, index) => (
                     <SkillCard labelKey={data.labelKey} items={data.items} key={index}></SkillCard>
                 ))}
             </div>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                <Card className={"flex mt-8 justify-center w-max flex-row"}>
-                    {languages.map((language, index) => {
-                        return (
-                            <CardContent className={"flex items-center gap-2"} key={index}>
-                                {language.label}
-                                <Avatar className={"w-5 h-5 overflow-hidden"}>
-                                    <AvatarImage src={language.icon}></AvatarImage>
-                                    <AvatarFallback>{language.icon}</AvatarFallback>
-                                </Avatar>
-                            </CardContent>
-                        );
-                    })}
+            <div className="flex justify-center">
+                <Card className={"mt-8 justify-center max-w-max w-fit flex flex-row flex-wrap"}>
+                    {languages.map((language, index) => (
+                        <CardContent className={"flex items-center gap-2"} key={index}>
+                            {language.label}
+                            <Avatar className={"w-5 h-5 overflow-hidden"}>
+                                <AvatarImage src={language.icon}></AvatarImage>
+                                <AvatarFallback>{language.icon}</AvatarFallback>
+                            </Avatar>
+                        </CardContent>
+                    ))}
                 </Card>
             </div>
         </div>
