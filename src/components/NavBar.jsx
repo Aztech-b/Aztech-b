e2b9212c92a1ad57f919dbfb25bf0baf1766f2bf
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-08-01 16:37:52 +0500

    desctop navbar changed to motion navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,17 @@
-import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    navigationMenuTriggerStyle,
-    NavigationMenuViewport,
-} from "@/src/components/ui/navigation-menu";
 import { motion } from "motion/react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router";
 import useNavBarAnimationSetup from "../hooks/useNavBarAnimationSetup";
+import { Button } from "./ui/button";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
+    MotionNavigationMenu,
+    MotionNavigationMenuContent,
+    MotionNavigationMenuItem,
+    MotionNavigationMenuLink,
+    MotionNavigationMenuList,
+    MotionNavigationMenuTrigger,
+} from "./unlumen-ui/motion-navigation-menu";
 
 function NavBar() {
-    const { t, i18n } = useTranslation("translation", { keyPrefix: "navBar" });
     const { hidden } = useNavBarAnimationSetup();
 
     return (
@@ -27,46 +19,81 @@ function NavBar() {
             className="navBar relative"
             animate={{ y: hidden ? "calc(-1 * var(--navbar-height))" : 0, opacity: hidden ? 0 : 1 }}
         >
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                            <Link to="/">{t("home")}</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                            <Link to="/my-poems">{t("myPoems")}</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
+            <DesktopNavBar className={"md:block hidden"}></DesktopNavBar>
+        </motion.div>
+    );
+}
 
-                    <DropdownMenu>
-                        <DropdownMenuTrigger className={navigationMenuTriggerStyle()}>
-                            {t("language.header")}
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <DropdownMenuGroup>
-                                <DropdownMenuItem
+function MobileNavBar({ className }) {
+    return <></>;
+}
+
+function DesktopNavBar({ className }) {
+    const { t, i18n } = useTranslation("translation", { keyPrefix: "navBar" });
+    const contentClassName = "bg-my-accent/10 rounded-lg my-accent-1 my-accent-primary/15";
+
+    return (
+        <MotionNavigationMenu
+            className={className}
+            viewportClassName="bg-card border-none shadow-none"
+            springStiffness={350}
+            springDamping={32}
+        >
+            <MotionNavigationMenuList highlightClassName={"bg-my-accent/50 rounded-lg"}>
+                <MotionNavigationMenuItem value={"home"}>
+                    <MotionNavigationMenuTrigger>{t("home")}</MotionNavigationMenuTrigger>
+                    <MotionNavigationMenuContent highlightClassName={contentClassName}>
+                        <div className="grid grid-cols-3">
+                            <MotionNavigationMenuLink to="/">{t("home")}</MotionNavigationMenuLink>
+                            <MotionNavigationMenuLink to="/#about">About</MotionNavigationMenuLink>
+                            <MotionNavigationMenuLink className={"whitespace-nowrap"} to="/#toolsAndSkills">
+                                Tools & Skills
+                            </MotionNavigationMenuLink>
+                            <MotionNavigationMenuLink className={"whitespace-nowrap"} to="/#featured-projects">
+                                Featured Projects
+                            </MotionNavigationMenuLink>
+                            <MotionNavigationMenuLink to="/#Contact">Contact</MotionNavigationMenuLink>
+                        </div>
+                    </MotionNavigationMenuContent>
+                </MotionNavigationMenuItem>
+                <MotionNavigationMenuItem value={"my-poems"}>
+                    <MotionNavigationMenuTrigger>{t("myPoems")}</MotionNavigationMenuTrigger>
+                    <MotionNavigationMenuContent highlightClassName={contentClassName}>
+                        <MotionNavigationMenuLink className={"whitespace-nowrap"} to={"/my-poems"}>
+                            {t("myPoems")}
+                        </MotionNavigationMenuLink>
+                    </MotionNavigationMenuContent>
+                </MotionNavigationMenuItem>
+
+                <MotionNavigationMenuItem value={"change-language"}>
+                    <MotionNavigationMenuTrigger>Change Language</MotionNavigationMenuTrigger>
+                    <MotionNavigationMenuContent highlightClassName={contentClassName}>
+                        <div className="flex">
+                            <MotionNavigationMenuLink asChild={true}>
+                                <Button
+                                    variant="ghost"
                                     onClick={() => {
                                         i18n.changeLanguage("en");
                                     }}
                                 >
                                     {t("language.en")}
-                                </DropdownMenuItem>
-                                <DropdownMenuItem
+                                </Button>
+                            </MotionNavigationMenuLink>
+                            <MotionNavigationMenuLink asChild={true}>
+                                <Button
+                                    variant="ghost"
                                     onClick={() => {
                                         i18n.changeLanguage("ru");
                                     }}
                                 >
                                     {t("language.ru")}
-                                </DropdownMenuItem>
-                            </DropdownMenuGroup>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </NavigationMenuList>
-                <NavigationMenuViewport></NavigationMenuViewport>
-            </NavigationMenu>
-        </motion.div>
+                                </Button>
+                            </MotionNavigationMenuLink>
+                        </div>
+                    </MotionNavigationMenuContent>
+                </MotionNavigationMenuItem>
+            </MotionNavigationMenuList>
+        </MotionNavigationMenu>
     );
 }
 
